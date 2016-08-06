93bc6ae15853683a91bccaec088ed8be935eb1ee
Dhananjay Vasa <vasa.dhananjay@gmail.com>
2016-08-06 19:31:43 -0400

    Initial Commit

---
+++ Startup.py
@@ -0,0 +1,69 @@
+day = None
+
+while (day == None): 
+    try:
+        day = int(input("Select day: "))
+    except:
+        print("Invalid input")
+
+if (day == 1):
+    print('Day 1')
+    import Day1
+elif (day == 2):
+    pass
+elif (day == 3):
+    pass
+elif (day == 4):
+    pass
+elif (day == 5):
+    pass
+elif (day == 6):
+    pass
+elif (day == 7):
+    pass
+elif (day == 8):
+    pass
+elif (day == 9):
+    pass
+elif (day == 10):
+    pass
+elif (day == 11):
+    pass
+elif (day == 12):
+    pass
+elif (day == 13):
+    pass
+elif (day == 14):
+    pass
+elif (day == 15):
+    pass
+elif (day == 16):
+    pass
+elif (day == 17):
+    pass
+elif (day == 18):
+    pass
+elif (day == 19):
+    pass
+elif (day == 20):
+    pass
+elif (day == 21):
+    pass
+elif (day == 22):
+    pass
+elif (day == 23):
+    pass
+elif (day == 24):
+    pass
+elif (day == 25):
+    pass
+elif (day == 26):
+    pass
+elif (day == 27):
+    pass
+elif (day == 28):
+    pass
+elif (day == 29):
+    pass
+elif (day == 30):
+    pass
